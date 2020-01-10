6dff052c782009ee8f04ed72b36cde44a5949c42
jesus2045 <59747471+jesus2045@users.noreply.github.com>
2020-01-10 19:07:33 -0400

    Initial commit

---
+++ Webmvc5/Models/EmpleadoesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webmvc5.Models
+{
+    public class EmpleadoesController : Controller
+    {
+        private crudmvcEntities db = new crudmvcEntities();
+
+        // GET: Empleadoes
+        public ActionResult Index()
+        {
+            return View(db.Vempleados.ToList());
+        }
+
+        public ActionResult departamentos()
+        {
+            var items = db.Departamentos.ToList();
+            if(items !=null)
+            {
+                ViewBag.data = items;
+            }
+           
+            return View();
+
+
+        }
+
+        // GET: Empleadoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Create
+        public ActionResult Create()
+        {
+            var items = db.Departamentos.ToList();
+            if (items != null)
+            {
+                ViewBag.data = items;
+            }
+            return View();
+        }
+
+        // POST: Empleadoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,apellido,sueldo,idDepartamento,fechaIngreso")] Empleado empleado)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //int dpeto = ViewBag
+                string _midptoid = Request.Form["nombre"].ToString();
+                string [] myid = _midptoid.Split(',');
+                int idDpto = Int32.Parse(myid[1]);
+                empleado.idDepartamento = idDpto;
+                db.Empleado.Add(empleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleadoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,apellido,sueldo,idDepartamento,fechaIngreso")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleadoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+            db.Empleado.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Webmvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webmvc5.Startup))]
+namespace Webmvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
